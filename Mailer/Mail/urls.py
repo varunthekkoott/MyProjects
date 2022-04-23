9e9dfcfaf26bf2db906387ee1bea5cdd13f7f0c9
Varun Thekkoott <varun.thekkoott@gmail.com>
2022-04-23 21:23:49 +0530

    Removed Some Slashes

--- Mailer/Mail/urls.py
+++ Mailer/Mail/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('login/index/<int:id>/', views.pick, name='pick'),
     path('signup/', views.signup, name='signup'),
     path('check/', views.signcheck, name='signup'),
-    path('/login/index/<int:id>/inbox/', views.inbox, name='inbox'),
-    path('/login/index/<int:id>/inbox/<int:num>', views.mail, name='mail'),
-    path('/login/index/<int:id>/sendmail/', views.sendmail, name='sendmail'),
-    path('/login/index/<int:id>/sendmail/check', views.checksendmail, name='checksendmail')
+    path('login/index/<int:id>/inbox/', views.inbox, name='inbox'),
+    path('login/index/<int:id>/inbox/<int:num>', views.mail, name='mail'),
+    path('login/index/<int:id>/sendmail/', views.sendmail, name='sendmail'),
+    path('login/index/<int:id>/sendmail/check', views.checksendmail, name='checksendmail')
 ]
